3c0167b08453e8720ff0ed4d3fbdaa751939459f
Yutaka HARA <yutaka.hara+github@gmail.com>
2008-12-30 17:34:58 +0900

    new: /tricks

--- resources/map.rb
+++ resources/map.rb
@@ -2,6 +2,15 @@ module Jugti
   module Resources
     class Map
       include Waves::Resources::Mixin
+
+      on(:get, []) { 
+        redirect "/tricks" 
+      }
+
+      on(:get, ["tricks"]) {
+        view(:trick).list(:tricks => [])
+      }
+
       
     end
   end
