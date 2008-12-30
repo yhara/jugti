639e04461819b5f8bcf6a79351c43d5e47bb2d33
Yutaka HARA <yutaka.hara+github@gmail.com>
2008-12-31 01:49:40 +0900

    new: /trick/n

--- resources/trick.rb
+++ resources/trick.rb
@@ -3,7 +3,12 @@ module Jugti
     class Trick < Default
 
       on(:get, ["tricks"]) do
-        view.list(:tricks => controller(:trick).all )
+        view.list(:tricks => controller.all )
+      end
+      
+      on(:get, ["trick", :trick_id]) do
+        trick = model.find(:id => captured.trick_id)
+        view.show(:trick => trick)
       end
       
     end
