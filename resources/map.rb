11d5bb000e197445247c362c40be157086c7436f
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-01-02 02:00:24 +0900

    new: show tricks in top page

--- resources/map.rb
+++ resources/map.rb
@@ -4,10 +4,14 @@ module Jugti
       include Waves::Resources::Mixin
 
       on(:get, []) do
+        tricks = Jugti::Models::Trick.limit(10).to_a
+        tricks.push nil if tricks.size < 10
+
         records = Jugti::Models::Record.
                     reverse_order(:updated_at).
-                    limit(3)
-        view.top(:records => records)
+                    limit(10)
+
+        view.top(:tricks => tricks, :records => records)
       end
 
       on(true, ["tricks"])      { to :trick }
