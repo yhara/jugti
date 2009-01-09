6d47c62826c83c208bc129762909379146a1c7a7
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-01-09 16:58:34 +0900

    new: footer

--- startup.rb
+++ startup.rb
@@ -2,6 +2,8 @@ require 'foundations/classic'
 require 'layers/orm/providers/sequel'
 
 module Jugti
+  VERSION = "0.1"
+
   include Waves::Foundations::Classic
   include Waves::Layers::ORM::Sequel
 end
